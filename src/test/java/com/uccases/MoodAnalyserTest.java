b5f1be437a134bd25089db8ed528e5c3317f3c66
purushothaman-source <purushoth99jun@gmail.com>
2021-01-19 10:27:37 +0530

    ADDED : functionality to return happy in any mood

--- src/test/java/com/uccases/MoodAnalyserTest.java
+++ src/test/java/com/uccases/MoodAnalyserTest.java
@@ -5,9 +5,17 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenMessage_ShouldReturn_ProperMood() {
+    public void givenSadMessage_ShouldReturn_Sad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I'm in sad Mood");
         Assert.assertEquals("SAD", mood);
     }
+    @Test
+    public void givenAnyMessage_ShouldReturn_Happy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I'm in any Mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
+
+
 }
