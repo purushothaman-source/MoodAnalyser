47b9a38407b18c5d9e29c316d3a9b05426d55c22
purushothaman-source <purushoth99jun@gmail.com>
2021-01-19 10:18:07 +0530

    ADDED : functionality to test sad message

--- src/test/java/com/uccases/MoodAnalyserTest.java
+++ src/test/java/com/uccases/MoodAnalyserTest.java
@@ -1,15 +1,13 @@
 package com.uccases;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_ShouldReturn_ProperMood() {
-        MoodAnalyser moodAnalyser=new  MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is sad message");
-        Assert.assertThat(mood,CoreMatchers.is("SAD"));
-
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I'm in sad Mood");
+        Assert.assertEquals("SAD", mood);
     }
 }
