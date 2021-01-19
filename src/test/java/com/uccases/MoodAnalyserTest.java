00c12a35b19f3a6fe388a02905ba8993f715e0aa
purushothaman-source <purushoth99jun@gmail.com>
2021-01-19 10:40:37 +0530

    Refractored : refract message  to Constructor

--- src/test/java/com/uccases/MoodAnalyserTest.java
+++ src/test/java/com/uccases/MoodAnalyserTest.java
@@ -6,16 +6,15 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     @Test
     public void givenSadMessage_ShouldReturn_Sad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I'm in sad Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("Im in sad mood ");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", mood);
     }
     @Test
     public void givenAnyMessage_ShouldReturn_Happy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I'm in any Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I'm in any Mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", mood);
     }
-
 
 }
